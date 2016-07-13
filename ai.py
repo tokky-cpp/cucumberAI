1423eca5a370949d49d6e6278553bac80a1018c5
tokky <tokky@okadomelab-no-MacBook-Air.local>
2016-07-13 13:59:20 +0900

    use evaluation function

--- ai.py
+++ ai.py
@@ -1,24 +1,47 @@
 
 #coding=utf-8
-#2016-07-11
+#2016-07-13
 #5本のキュウリAI 
-#ver 0.0
-#ルールにしたがって出しているだけ。
-#v1
+#ver 1.0
+#評価関数を導入
+
+
+def evaluation(nl): #スコアの低いほうが良い
+    score = 0
+    for i in nl:
+        score += min(i-1,15-i)
+    return score
+
+
 def answer(nl, n): #nl:手持ちの数字リスト n:場に出ている数字
     if n is None: n=1 #もし場に何も出ていなければ１が出ていることにしておく
     nl.sort() 
 
-    for i in nl:
-        if n<=i: #小さい順に見ていってもし出せる数字があれば出す。
-            ret = i
-            nl.remove(i)
-#            print("remove"+str(i))
-            return(nl,ret)
-   
-#場の数字以上の数字を持っていなかったときの処理
+    #一番小さいものを出すのをベストにしておく(とりあえず)
+    best_nl = nl[1:]
+    best_score = evaluation(nl[1:])
     ret = nl[0]
-    nl.remove(nl[0])
- #   print("remove"+str(i))
-    return(nl,ret)
+    print(best_nl,best_score,ret)
+    
+
+    for i in range(len(nl)-1):
+        if n>nl[i]: #場に出ているほうが大きかったらだめ
+            continue
+        else: #もし場に出ている数字以上なら
+            nl_=nl[:i]+nl[i+1:] #1つ取り除いて
+            e = evaluation(nl_) #評価関数に入れる
+            print(nl_,e,nl[i])
+            if(best_score > e): #もしスコアが良ければ上書き
+                best_nl = nl_
+                best_score = e
+                ret = nl[i]
+
+#            print("remove"+str(i))
+    return(best_nl,ret)
+   
+
+
+
+
+
     
