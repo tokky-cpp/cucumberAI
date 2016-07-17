eea0ce04b3f91d40826fa9e5df137300040107c8
tokky <tokky@okadomelab-no-MacBook-Air.local>
2016-07-17 12:30:35 +0900

    add init(deal)

--- init.py
+++ init.py
@@ -5,7 +5,7 @@ import random
 def deal(l): #l枚ランダムに生成。リスト形式で返す。
     nl=[]
     for i in range(l):
-        nl.append(random.randint(0,15))
+        nl.append(random.randint(1,15))
         nl.sort()
     return nl
 
